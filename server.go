9dbe0d2dd45c3c254faf3f01d18d38e416431698
Md. Abu Sayeed Mondal <sayeedmondal1412@gmail.com>
2021-11-02 22:11:47 +0600

    done all

--- server.go
+++ server.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"io/ioutil"
+
 	"path/filepath"
+
+	"log"
+
+	"strings"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -17,6 +23,44 @@ func main() {
 
   	app.Get("/iframe", func(c *fiber.Ctx) error {
     	return c.SendFile(filepath.FromSlash("iframe.html"))
+  	})
+
+	app.Post("/change-iframe", func(c *fiber.Ctx) error {
+		//payload
+		payload := struct {
+			Content string `json:"content"`
+		}{}
+			
+		//parse body
+		if err := c.BodyParser(&payload); err != nil {
+			return err
+		}
+
+		//read iframe file
+		input, err := ioutil.ReadFile(filepath.FromSlash("iframe.html"))
+		
+		//log error
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		//get all lines in a array
+		lines := strings.Split(string(input), "\n")
+
+		//check text
+        for i, line := range lines {
+                if strings.Contains(line, "<h1>") {
+                        lines[i] = "<h1>" + payload.Content + "</h1>"
+                }
+        }
+        output := strings.Join(lines, "\n")
+        err = ioutil.WriteFile(filepath.FromSlash("iframe.html"), []byte(output), 0644)
+        if err != nil {
+                log.Fatalln(err)
+        }
+
+		return c.JSON(payload)
+
   	})
 
   	app.Listen(":3000")
